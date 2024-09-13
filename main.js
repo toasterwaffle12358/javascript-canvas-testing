6a8ef03c6691874d37c20ad3f81fbbe562938acf
Avi Lieberman <115607216+toasterwaffle12358@users.noreply.github.com>
2024-09-13 15:23:50 -0700

    added crazy cool effects

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ let colorslist = ["red", "orange", "yellow", "green", "blue", "purple"]
 let mouseX = 0;
 let mouseY = 0;
 
+let hue = 0
 
 ctx.fillStyle = "black";
 ctx.fillRect(0, 0, WIDTH, HEIGHT);
@@ -23,20 +24,46 @@ function updateMousePosition(event) {
 
 function draw() {
 
+
+    /*
     for (let i = 0; i < 100; i++) {
         ctx.fillStyle = `hsl(${i*10} 50% ${i}%)`;
         ctx.fillRect(i*((mouseX-(WIDTH/2))/50)+(WIDTH/2)-(i/2), i*((mouseY-(HEIGHT/2))/50)+(HEIGHT/2)-(i/2), i, i);
     }
+    */
+    
+    
+    ctx.fillStyle = `hsl(${hue} 100% 80%)`
+    ctx.fillRect(mouseX, mouseY, 70, 70);
+    ctx.filter = "blur(20px) contrast(100%)"
+    ctx.drawImage(canvas, -WIDTH*0.003, -HEIGHT*0.003, WIDTH*1.006, HEIGHT*1.006)
+    ctx.filter = "none"
+
     console.log("function ran")
+    ctx.fillStyle = "rgb( 0 0 0 / 1%";
+    ctx.fillRect(0, 0, WIDTH, HEIGHT);
+
+    console.log("drawn")
+
 }
 
 canvas.addEventListener('mousemove', (event) => {
     updateMousePosition(event);
+    hue = hue +10
+    hue = hue%360
+
+    /*
     ctx.fillStyle = "rgb( 0 0 0 / 2%";
     ctx.fillRect(0, 0, WIDTH, HEIGHT);
+    ctx.drawImage(canvas, -WIDTH*0.003, -HEIGHT*0.003, WIDTH*1.006, HEIGHT*1.006)
     draw();
     console.log("drawn")
+    */
 });
+
+setInterval(draw, 10);
+
+
 canvas.addEventListener('mousedown', () => {
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, WIDTH, HEIGHT);
