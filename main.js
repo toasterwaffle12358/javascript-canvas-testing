168f63343a52a2de19ad79c83b6fb3418769590f
Avi Lieberman <115607216+toasterwaffle12358@users.noreply.github.com>
2024-09-14 12:08:57 -0700

    added high contrast mode

--- main.js
+++ main.js
@@ -6,8 +6,6 @@ const HEIGHT = window.innerHeight
 ctx.canvas.width = WIDTH
 ctx.canvas.height = HEIGHT
 
-let colorslist = ["red", "orange", "yellow", "green", "blue", "purple"]
-
 let mouseX = 0;
 let mouseY = 0;
 
@@ -15,6 +13,8 @@ let hue = 0
 
 ctx.fillStyle = "black";
 ctx.fillRect(0, 0, WIDTH, HEIGHT);
+
+let highContrast = false
 
 function updateMousePosition(event) {
     const rect = canvas.getBoundingClientRect();
@@ -35,7 +35,11 @@ function draw() {
     
     ctx.fillStyle = `hsl(${hue} 100% 80%)`
     ctx.fillRect(mouseX, mouseY, 70, 70);
-    ctx.filter = "blur(20px) contrast(100%)"
+    if (highContrast == false) {
+        ctx.filter = "blur(20px)"
+    } else {
+        ctx.filter = "blur(20px) contrast(100000%)"
+    }
     ctx.drawImage(canvas, -WIDTH*0.003, -HEIGHT*0.003, WIDTH*1.006, HEIGHT*1.006)
     ctx.filter = "none"
 
@@ -49,7 +53,7 @@ function draw() {
 
 canvas.addEventListener('mousemove', (event) => {
     updateMousePosition(event);
-    hue = hue +10
+    hue = hue +5
     hue = hue%360
 
     /*
@@ -65,8 +69,9 @@ setInterval(draw, 10);
 
 
 canvas.addEventListener('mousedown', () => {
-    ctx.fillStyle = "black";
-    ctx.fillRect(0, 0, WIDTH, HEIGHT);
+    //ctx.fillStyle = "black";
+    //ctx.fillRect(0, 0, WIDTH, HEIGHT);
+    highContrast = !highContrast
 })
 
 draw()
