f0082533e9d2cd098c22790c4b99698fc8cf2e44
Avi Lieberman <115607216+toasterwaffle12358@users.noreply.github.com>
2024-09-23 15:30:51 -0700

    new page, so awesome

---
+++ page2.js
@@ -0,0 +1,16 @@
+var canvas = document.getElementById('canvas')
+var ctx = canvas.getContext('2d')
+    
+const WIDTH = window.innerWidth
+const HEIGHT = window.innerHeight
+ctx.canvas.width = WIDTH
+ctx.canvas.height = HEIGHT
+
+let mouseX = 0;
+let mouseY = 0;
+
+function updateMousePosition(event) {
+    const rect = canvas.getBoundingClientRect();
+    mouseX = event.clientX - rect.left;
+    mouseY = event.clientY - rect.top;
+}
