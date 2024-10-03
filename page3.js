9717496ce6ce9e5173047fea2998cb3cba34efc5
Avi Lieberman <115607216+toasterwaffle12358@users.noreply.github.com>
2024-10-03 15:20:40 -0700

    updated page3 changed what it does

--- page3.js
+++ page3.js
@@ -13,31 +13,27 @@ var MouseDownPos = 0;
 
 let mousePosListX = []
 let mousePosListY = []
+canvas.requestPointerLock();
 
 function updateMousePosition(event) {
     const rect = canvas.getBoundingClientRect();
+    mouseX = event.clientX - rect.left;
+    mouseY = event.clientY - rect.top;
     if (MouseDownPos == 1) {
-        mouseX = event.clientX - rect.left;
-        mouseY = event.clientY - rect.top;
         mousePosListX.push(mouseX)
         mousePosListY.push(mouseY)
     }
 
-    if ( mousePosListX.length >= 10) {
+    if ( mousePosListX.length >= 100) {
         mousePosListX.shift()
         mousePosListY.shift()
     }
 
+
 }
 
 function draw() {
-    ctx.filter = "contrast(5000%) hue-rotate(19deg)"
-    ctx.drawImage(canvas, 0, 0, WIDTH/2, HEIGHT/2)
-    ctx.drawImage(canvas, WIDTH/2, 0, WIDTH/2, HEIGHT/2)
-    ctx.drawImage(canvas, 0, HEIGHT/2, WIDTH/2, HEIGHT/2)
-    ctx.drawImage(canvas, WIDTH/2, HEIGHT/2, WIDTH/2, HEIGHT/2)
-    ctx.drawstyle
-
+    ctx.filter = "contrast(100%) hue-rotate(19deg) invert(1)"
     ctx.lineWidth = 50;
     ctx.beginPath()
     ctx.moveTo(mousePosListX[0], mousePosListY[0]);
@@ -45,13 +41,15 @@ function draw() {
         ctx.lineTo(mousePosListX[i-1], mousePosListY[i-1])
         ctx.stroke()
     }
+    let tempcanvas = canvas
+    ctx.drawImage(tempcanvas, WIDTH/2-mouseX, HEIGHT/2-mouseY, mouseX, mouseY)
+    ctx.drawImage(tempcanvas, WIDTH/2, HEIGHT/2-mouseY, mouseX, mouseY)
+    ctx.drawImage(tempcanvas, WIDTH/2-mouseX, HEIGHT/2, mouseX, mouseY)
+    ctx.drawImage(tempcanvas, WIDTH/2, HEIGHT/2, mouseX, mouseY)
 
-
-    ctx.fillStyle = "rgb( 255 400 300 / 0.2%";
+    ctx.fillStyle = "rgb( 255 400 300 / 10%";
     ctx.fillRect(0, 0, WIDTH, HEIGHT);
     requestAnimationFrame(draw)
-    if (MouseDownPos == 0) {
-    }
 }
 
 canvas.addEventListener('mousemove', (event) => {
@@ -67,6 +65,25 @@ canvas.addEventListener('mousedown', () => {
 canvas.addEventListener('mouseup', () => {
     MouseDownPos = 0
 })
+
+document.addEventListener('keydown', (event) => {
+    if (isMouseCaptured && event.key === 'Escape') {
+        document.exitPointerLock(); // Exit pointer lock
+    }
+});
+
+canvas.addEventListener('touchmove', (event) => {
+    event.preventDefault(); // Prevent scrolling
+    const touch = event.touches[0];
+    updateMousePosition(touch);
+});
+
+document.addEventListener('touchmove', (event) => {
+}, { passive: false });
+document.addEventListener('gesturestart', (event) => {
+}, { passive: false });
+document.addEventListener('touchstart', (event) => {
+}, { passive: false });
 
 
 requestAnimationFrame(draw)
