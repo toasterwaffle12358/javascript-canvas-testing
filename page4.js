e5ce47b09a43b4e314027ef8babfe8ec5f5571f0
Avi Lieberman <115607216+toasterwaffle12358@users.noreply.github.com>
2024-10-23 14:27:58 -0700

    mouse speed controls ball speed

--- page4.js
+++ page4.js
@@ -7,29 +7,59 @@ ctx.canvas.width = WIDTH
 ctx.canvas.height = HEIGHT
 
 let velocityX = 10
-let velocityY = 0
+let velocityY = 10
 let ballX = WIDTH/2
 let ballY = HEIGHT/2
-let gravity = 1
+let pastBallX = WIDTH/2
+let pastBallY = HEIGHT/2
+let gravity = 0
 
+let mouseX = 0
+let mouseY = 0 
+
+hue = 1
 
 ctx.fillStyle = "rgb(0 0 0%)"
 ctx.fillRect(0, 0, WIDTH, HEIGHT)
 
+function updateMousePosition(event) {
+    const rect = canvas.getBoundingClientRect();
+    mouseX = event.movementX
+    mouseY = event.movementY
+    velocityX += mouseX*100000
+    velocityY += mouseY*100000
+}
+
+
+
 function draw() {
-    ctx.fillStyle = "rgb(0 0 0 /10%)"
+    ctx.fillStyle = "rgb(0 0 0 /1%)"
     ctx.fillRect(0, 0, WIDTH, HEIGHT)
 
     ctx.fillStyle = "rgb(100 100 100)"
     ctx.fillRect(0, HEIGHT-100, WIDTH, 100)
-    ctx.fillRect(WIDTH/4, HEIGHT-300, WIDTH/2, 50)
+    //ctx.fillRect(WIDTH/4, HEIGHT-300, WIDTH/2, 50)
 
-    ctx.fillStyle = "rgb(100 200 100)"
-    ctx.fillRect(ballX-5, ballY-5, 10, 10)
+    pastBallX = ballX
+    pastBallY = ballY
+    
     velocityY += gravity
+    velocityX = velocityX*0.999
+    velocityY = velocityY*0.999
     ballX += velocityX
     ballY += velocityY
+
+    ctx.strokeStyle = `hsl(${hue} 100% 50%)`
+    hue += 1
+    hue = hue % 360
+
+    ctx.lineWidth = 15;
+    ctx.beginPath()
+    ctx.moveTo(pastBallX, pastBallY);
+    ctx.lineTo(ballX, ballY)
+    ctx.stroke()
     
+    /*
     if (ballY >= HEIGHT-305 && ballY <= HEIGHT-255) {
         if (ballX <= (WIDTH*3/4)+5) {
             ballY = HEIGHT-305
@@ -37,18 +67,41 @@ function draw() {
             velocityX = velocityX*0.995
         }
     }
+    */
     
     if (ballY >= HEIGHT-105) {
         ballY = HEIGHT-105
-        velocityY = -velocityY*1
-        velocityX = velocityX*0.995
+        velocityY = -velocityY*0.95
+        velocityX = velocityX*0.99
     }
-    if (ballX > WIDTH-5 || ballX <= 0 ) {
+    if (ballX > WIDTH-5) {
         velocityX = -velocityX
+        ballX = WIDTH-5
+    }
+    if (ballX <= 0 ) {
+        velocityX = -velocityX
+        ballX = 5
+    }
+    if (ballY < 0) {
+        velocityY = -velocityY
+        ballY = 5
     }
 
     requestAnimationFrame(draw)
 }
 
 
+canvas.addEventListener('mousemove', (event) => {
+    updateMousePosition(event);
+});
+
+document.addEventListener('keydown', (event) => {
+    if (isMouseCaptured && event.key === 'Escape') {
+        document.exitPointerLock(); // Exit pointer lock
+    }
+});
+
+canvas.addEventListener('click', function() {
+    canvas.requestPointerLock();
+});
 requestAnimationFrame(draw)
