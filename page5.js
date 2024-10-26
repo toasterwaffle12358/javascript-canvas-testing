e7171b31d45eeb3919282799f3178916cbdfa27b
toasterwaffle12358 <avi.y.lieberman@gmail.com>
2024-10-25 23:32:32 -0700

    accurate ray casting and player movement

--- page5.js
+++ page5.js
@@ -1,32 +1,50 @@
-
-
-
-
 ctx.fillStyle = "rgb( 0 0 0)";
 //ctx.fillRect(WIDTH/2, HEIGHT/2, 100, 100);
 
-let worldMap = [
-    [true, true, true, true, true, true],
-    [true, false, true, true, false, true],
-    [true, false, true, false, false, true],
-    [true, false, false, false, false, true],
-    [true, false, false, false, false, true],
-    [true, true, true, true, true, true],
-]
 
-let playerPosition = [1,1]
-let pointpos = [0,1]
+let playerPosition = [6,5]
+let pointPosList = [[0,0],[0,2],[2,0],[2,2]]
+
+
+let distance = 0
+let angle = 0
+let screenXPos = 0
+
 
 function getDistances() {
+    for (i in pointPosList) {
+        distance = Math.sqrt(Math.pow(playerPosition[0] - pointPosList[i][0], 2)+Math.pow(playerPosition[1] - pointPosList[i][1], 2))
+        angle = Math.atan((playerPosition[1]- pointPosList[i][1])/(playerPosition[0]-pointPosList[i][0]))
+        if (angle >= 0) {
+            screenXPos = ((Math.PI-angle)/(Math.PI))*WIDTH
+        } else {
+            screenXPos = ((-angle)/(Math.PI))*WIDTH
+        }
 
+        if (screenXPos > 0 && screenXPos < WIDTH) {
+            ctx.fillRect(screenXPos, (HEIGHT/2)-((HEIGHT/distance)/2), 2, HEIGHT/distance);
+        }
+        
+    }
 }
-let distance = Math.sqrt(Math.pow(playerPosition[0]+ pointpos[1], 2)+Math.pow(playerPosition[1], 2))
-let angle = Math.atan((playerPosition[1])/(playerPosition[0]+pointpos[1]))
-let screenXPos = ((Math.PI-angle)/(Math.PI))*WIDTH
+//getDistances()
 
+
+function draw() {
+    ctx.fillStyle = "rgb( 255 255 255)";
+    ctx.fillRect(0, 0, WIDTH, HEIGHT);
+    ctx.fillStyle = "rgb( 0 0 0)";
+    getDistances()
+    requestAnimationFrame(draw)
+}
+
+
+/*
 if (screenXPos > 0 && screenXPos < WIDTH) {
     ctx.fillRect(screenXPos, (HEIGHT/2)-((HEIGHT/distance)/2), 2, HEIGHT/distance);
 }
+*/
+
 
 console.log("distance:")
 console.log(distance)
@@ -35,8 +53,26 @@ console.log(angle)
 console.log("screen x pos:")
 console.log(screenXPos)
 
+document.addEventListener('keydown', (event) => {
+    switch (event.key) {
+        case 'Escape' :
+            if (isMouseCaptured) {
+                document.exitPointerLock(); // Exit pointer lock
+            }
+            break
+        case 'ArrowDown' :
+            playerPosition[1] += -0.1
+            break
+        case 'ArrowUp' :
+            playerPosition[1] += 0.1
+            break
+        case 'ArrowLeft' :
+            playerPosition[0] += -0.1
+            break
+        case 'ArrowRight' :
+            playerPosition[0] += 0.1
+            break
+    }
+});
 
-
-for (let i = 0; i < WIDTH; i++) {
-    //ctx.fillRect(i, (HEIGHT/2)-(Math.sin(i/50)*50), 1, Math.sin(i/50)*100);
-}
+requestAnimationFrame(draw)
